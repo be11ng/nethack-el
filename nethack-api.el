90b2e769df39e3a7f6632c3b3a5bedec989e802c
sabetts <sabetts>
2000-09-10 05:49:22 +0000

    added the 'ch' param to the print-glyph function. ch is the default ascii character to display for the glyph.

--- nethack-api.el
+++ nethack-api.el
@@ -208,21 +208,20 @@
 ;; whatever the window- port wants (symbol, font, color, attributes,
 ;; ...there's a 1-1 map between glyphs and distinct things on the map).
 
-(defun nethack-api-print-glyph (window x y type offset glyph)
+(defun nethack-api-print-glyph (window x y type offset glyph ch)
   ""
-
   (set-buffer (nethack-get-buffer window))
-  (let ((ch))
-    (cond ((eq type 'monster) (setq ch ?@))
-	  ((eq type 'pet) (setq ch ?d))
-	  ((eq type 'invisible) (setq ch ?I))
-	  ((eq type 'detect) (setq ch ?D))
-	  ((eq type 'corpse) (setq ch ?%))
-	  ((eq type 'ridden) (setq ch ?R))
-	  ((eq type 'object) (setq ch ??))
-	  ((eq type 'cmap) (setq ch ?.))
-	  ((eq type 'zap-beam) (setq ch ??)))
-    (gamegrid-set-cell x y ch))
+;   (let ((ch))
+;     (cond ((eq type 'monster) (setq ch ?@))
+; 	  ((eq type 'pet) (setq ch ?d))
+; 	  ((eq type 'invisible) (setq ch ?I))
+; 	  ((eq type 'detect) (setq ch ?D))
+; 	  ((eq type 'corpse) (setq ch ?%))
+; 	  ((eq type 'ridden) (setq ch ?R))
+; 	  ((eq type 'object) (setq ch ??))
+; 	  ((eq type 'cmap) (setq ch ?.))
+; 	  ((eq type 'zap-beam) (setq ch ??)))
+    (gamegrid-set-cell x y ch)
   'void-fixme)
 
 
