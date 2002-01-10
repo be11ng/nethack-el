f8d2eb454988dfbe1eee93b173d0f4d16ba29459
rcyeske <rcyeske>
2002-01-10 21:21:44 +0000

    * nethack.el (nethack-map-glyph-face): Put in nethack-faces-faces group. (nethack-use-glyphs): Default to nil. (nethack-toggle-glyphs): New function.
    * nethack-api.el (nethack-api-display-nhwindow): Don't wait for a
    key on echo area messages.
    (nethack-menu-item-print, nethack-menu-toggle-item)
    (nethack-menu-toggle-item, nethack-menu-toggle-all-items)
    (nethack-menu-submit, nethack-api-add-menu): Handle prefix arg on
    menu selection.  From sabetts.
    
    * nethack-cmd.el (redraw-screen): Restore window configuration
    always, only redraw glyphs with a prefix arg.
    (defun-nethack-command): Expand cmdstr in test.
    
    * nethack-api.el (nethack-restore-window-configuration): Make the
    status window dedicated.

--- nethack.el
+++ nethack.el
@@ -1,7 +1,7 @@
 ;;; nethack.el -- run Nethack as an inferior process in Emacs
 ;;; Author: Ryan Yeske (rcyeske@vcn.bc.ca)
 ;;; Date: Sat Mar 18 11:31:52 2000
-;;; $Id: nethack.el,v 1.50 2001/12/21 03:17:14 sabetts Exp $
+;;; $Id: nethack.el,v 1.51 2002/01/02 02:58:56 sabetts Exp $
 ;;; Requires: a copy of Nethack 3.3.x with the lisp window port
 
 ;;; Commentary:
@@ -29,7 +29,7 @@
 (defface nethack-black-face
   `((t (:foreground "blue")))
   "nethack black face"
-  :group 'nethack-faces-faces)
+  :group 'nethack-faces)
 
 (defface nethack-red-face
   `((((type tty) (class color))
@@ -38,7 +38,7 @@
      (:foreground "red"))
     (t (:foreground "gray")))
   "nethack red"
-  :group 'nethack-faces-faces)
+  :group 'nethack-faces)
 
 (defface nethack-green-face
   `((((type tty) (class color))
@@ -186,12 +186,18 @@
 
 (defface nethack-map-glyph-face 
   `((t (:font "6x10")))
-  "nethack map face for keeping glyphs from 'seperating' due to the
+  "Nethack map face for keeping glyphs from separating due to the
 newlines being in a font with height > 16."
-  :group 'nethack-faces-faces)
+  :group 'nethack-faces)
 
-(defvar nethack-use-glyphs (display-images-p)
-  "If set to T, nethack will use XPMs to draw glyphs.")
+(defvar nethack-use-glyphs nil
+  "If non-nil, use XPMs to draw glyphs.")
+
+(defun nethack-toggle-glyphs ()
+  "Toggle the use of glyphs on the map."
+  (interactive)
+  (setq nethack-use-glyphs (not nethack-use-glyphs))
+  (nethack-command-redraw-screen 2))
 
 (defconst nethack-colors
   [nethack-black-face 		nethack-red-face
