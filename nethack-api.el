3cbe67467a551fe0aa5afd523f84248017346a7e
sabetts <sabetts>
2002-01-22 10:40:24 +0000

    * nethack-keys.el (nethack-accelerator-chars): add ?* to the vector.
    * nethack-api.el (nethack-api-choose-role): remove function
    (nethack-api-choose-race): likewise
    (nethack-api-choose-gender): likewise
    (nethack-api-choose-alignment): likewise
    (nethack-menu-toggle-item): adjust item regexp to include ?*
    (nethack-menu-toggle-all-items): likewise
    (nethack-menu-goto-next): likewise
    (nethack-menu-goto-prev): likewise
    (nethack-menu-submit): likewise
    (nethack-menu-cancel): likewise

--- nethack-api.el
+++ nethack-api.el
@@ -1,6 +1,6 @@
 ;;; nethack-api.el -- low level Emacs interface the lisp window-port
 ;;; of Nethack-3.3.x
-;;; $Id: nethack-api.el,v 1.62 2002/01/21 21:26:28 rcyeske Exp $
+;;; $Id: nethack-api.el,v 1.63 2002/01/22 10:24:11 sabetts Exp $
 
 ;;; originally a machine translation of nethack-3.3.0/doc/window.doc
 ;;; from the nethack src package.
@@ -747,7 +747,7 @@ actually toggled."
 	    (old-point (point)))
 	(goto-char (point-min))
 	(if (re-search-forward 
-	     (format "^%c \\([-+]\\|[0-9]+\\) .+$" last-command-event) 
+	     (format "^[%c] \\([-+]\\|[0-9]+\\) .+$" last-command-event) 
 	     nil t)
 	    (let ((inhibit-read-only t))
 	      (if (and count (> count 1))
@@ -769,7 +769,7 @@ actually toggled."
       (save-excursion
 	(let ((inhibit-read-only t))
 	  (goto-char (point-min))
-	  (while (re-search-forward "^[a-zA-Z] \\([-+]\\|[0-9]+\\) .+$" nil t)
+	  (while (re-search-forward "^[a-zA-Z*] \\([-+]\\|[0-9]+\\) .+$" nil t)
 	    (let ((value (match-string 1)))
 	      (if (string-equal value "-")
 		  (replace-match "+" nil nil nil 1)
@@ -780,7 +780,7 @@ actually toggled."
   (interactive)
   (let ((old-point (point)))
     (goto-char (line-end-position))
-    (goto-char (if (re-search-forward "^[a-zA-Z] [-+]\\|[0-9]+ .+$" nil t)
+    (goto-char (if (re-search-forward "^[a-zA-Z*] [-+]\\|[0-9]+ .+$" nil t)
 		   (line-beginning-position)
 		 old-point))))
 
@@ -789,7 +789,7 @@ actually toggled."
   (interactive)
   (let ((old-point (point)))
     (goto-char (line-beginning-position))
-    (goto-char (if (re-search-backward "^[a-zA-Z] [-+]\\|[0-9]+ .+$" nil t)
+    (goto-char (if (re-search-backward "^[a-zA-Z*] [-+]\\|[0-9]+ .+$" nil t)
 		   (line-beginning-position)
 		 old-point))))
 
@@ -801,7 +801,7 @@ displayed."
   (interactive)
   (goto-char (point-min))
   (let ((menu-data nil))
-    (while (re-search-forward "^\\([a-zA-Z]\\) \\([-+]\\|[0-9]+\\) .+$" nil t)
+    (while (re-search-forward "^\\([a-zA-Z*]\\) \\([-+]\\|[0-9]+\\) .+$" nil t)
       (let ((accelerator (match-string 1))
 	    (value (match-string 2))
 	    (identifier (get-text-property (point) 'nethack-id)))
@@ -822,7 +822,7 @@ displayed."
   (let ((inhibit-read-only t))
     (goto-char (point-min))
     ;; turn off all the options
-    (while (re-search-forward "^[a-zA-Z] \\([-+]\\|[0-9]+\\) .+$" nil t)
+    (while (re-search-forward "^[a-zA-Z*] \\([-+]\\|[0-9]+\\) .+$" nil t)
       (replace-match "-" nil nil nil 1)))
   (nethack-menu-submit))
 
