ff0067641f4462a3070823733a618fa727b563c9
Benjamin Yang <be11ng@protonmail.com>
2020-06-13 11:48:07 -0400

    Reword nethack-untar-nethack docstring
    The ``--strip-components'' is a flag, and saying that something ``relies on it''
    is a bit hard to parse.

--- nethack.el
+++ nethack.el
@@ -493,7 +493,7 @@ Currently, the two supported versions are 3.6.6 and 3.4.3."
 Untars the file nethack.tgz into TARGET-DIRECTORY using tar xzf.
 
 Note that this may be system specific to GNU tar and BSD tar,
-since it relies on using --strip-components."
+since it relies on using the flag --strip-components."
   (shell-command
    (concat "tar xzf nethack.tgz -C "
            target-directory
