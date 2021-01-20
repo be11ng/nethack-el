81c9501ba7386d8c24b7dc89f6ea2c8d97ebec73
Benjamin Yang <be11ng@protonmail.com>
2021-01-19 20:19:45 -0500

    Reorder nethack-options.el so it executes
    Since `nethack-options` calls `(nethack-options-parse)` to get a default value,
    `nethack-options-parse` should be defined before the definition of
    `nethack-options`.
    
    I also rearranged the other function so that it all "reads" a little bit nicer.

--- nethack-options.el
+++ nethack-options.el
@@ -48,16 +48,6 @@ parsing is also done on the Lisp-side of nethack-el."
   :type '(file)
   :group 'nethack)
 
-(defcustom nethack-options
-  (nethack-options-parse)
-  "Alist representing nethackrc options
-
-This can be edited through ‘custom-set’."
-  ;; TODO implement customize better
-  ;; :type '(alist :key-type string)
-  :type '(sexp)
-  :group 'nethack)
-
 
 
 (defconst nethack-options-fields
@@ -79,6 +69,94 @@ This can be edited through ‘custom-set’."
    nethack-options-fields))
 
 
+
+
+(defun nethack-options-parse-status-behav (behav)
+  (list 'behavior
+        behav))
+
+(defun nethack-options-parse-attr (attributes)
+  (list 'attributes
+        (if (string-match-p "[&+]" attributes)
+            (split-string attributes "[&+]" t "[ \t\n\r]+")
+          attributes)))
+
+(defun nethack-options-parse-hilite-status (params)
+  (let* ((ops (split-string params "/" t "[ \t\n\r]+"))
+         (field-name (pop ops))
+         (result (list field-name)))
+    (while ops
+      (setq result
+            (cons
+             (cond
+              ;; These two need to be handled a little differently
+              ((equal "condition" (car ops))
+               (list (cons 'condition
+                           (pop ops))   ; (cadr ops)
+                     (nethack-options-parse-attr (pop ops))))
+              ((string-suffix-p "-max" field-name)
+               (list (nethack-options-parse-attr (pop ops))))
+              ;; In most cases:
+              ((and (nethack-options-status-field-p field-name)
+                    (cdr ops))
+               (list (nethack-options-parse-status-behav (pop ops))
+                     (nethack-options-parse-attr (pop ops))))
+              ;; For something like: hilite_status:hitpoints/<=30%/red/normal
+              (t
+               (list '(else)
+                     (nethack-options-parse-attr (pop ops)))))
+             result)))
+    (reverse result)))
+
+(defun nethack-options-parse-menucolor (elem)
+  "Parse a nethackrc MENUCOLOR= line.
+
+Returns an alist entry of the options set."
+  (when (string-prefix-p "MENUCOLOR=" elem)
+    (setq elem (string-trim-left elem "[a-zA-Z]+=")))
+  ;; In case the regexp contains “=”
+  ;; TODO: Do this without using ‘reverse’
+  (setq elem (reverse (split-string elem "=" t)))
+  (setq attr (pop elem))
+  (if (stringp elem)
+      (setq elem (apply #'concat (reverse elem))))
+  (setq elem (car elem))                ; There should only be 1 item
+  ;; Remove quotes from either side of ‘elem’
+  (setq elem (substring elem 1 -1))
+  (list 'menucolor
+        elem
+        (nethack-options-parse-attr attr)))
+
+(defun nethack-options-parse-option-1 (elem)
+  ;; Cut out whitespace
+  (setq elem (string-trim-left elem))
+  ;; TODO: Make this more robust so if we also parse MENUCOLOR settings, then
+  ;; the ":" in the regexp won't confuse this
+  (if (string-match-p ":" elem)
+      ;; TODO: Set this up so it auto parses things like "hilite_status"
+      ;; The string trim regexp is copied from ‘string-trim-left’.
+      (let* ((option
+              (split-string elem ":" t "[ \t\n\r]+"))
+             (op (car option))
+             (params (cadr option)))
+        (cons
+         op
+         (cond
+          ((equal "hilite_status" op)
+           (nethack-options-parse-hilite-status params))
+          (t
+           (list params)))))
+    elem))
+
+(defun nethack-options-parse-option (elem)
+  "Parse a nethackrc OPTIONS= line.
+
+Returns a list of the options set."
+  ;; (when (string-prefix-p "OPTIONS=" elem)
+  ;;   (setq elem (string-trim-left elem "[a-zA-Z]+=")))
+  (mapcar
+   #'nethack-options-parse-option-1
+   (split-string (string-trim-left elem "[a-zA-Z]+=") "," t)))
 
 (defun nethack-options-parse ()
   "Return a parsed list of ‘nethack-options-file’.
@@ -114,97 +192,12 @@ Maybe I should have used eieio."
           (forward-line 1)))
       result)))
 
-(defun nethack-options-parse-option (elem)
-  "Parse a nethackrc OPTIONS= line.
-
-Returns a list of the options set."
-  ;; (when (string-prefix-p "OPTIONS=" elem)
-  ;;   (setq elem (string-trim-left elem "[a-zA-Z]+=")))
-  (mapcar
-   #'nethack-options-parse-option-1
-   (split-string (string-trim-left elem "[a-zA-Z]+=") "," t)))
-
-(defun nethack-options-parse-option-1 (elem)
-  ;; Cut out whitespace
-  (setq elem (string-trim-left elem))
-  ;; TODO: Make this more robust so if we also parse MENUCOLOR settings, then
-  ;; the ":" in the regexp won't confuse this
-  (if (string-match-p ":" elem)
-      ;; TODO: Set this up so it auto parses things like "hilite_status"
-      ;; The string trim regexp is copied from ‘string-trim-left’.
-      (let* ((option
-              (split-string elem ":" t "[ \t\n\r]+"))
-             (op (car option))
-             (params (cadr option)))
-        (cons
-         op
-         (cond
-          ((equal "hilite_status" op)
-           (nethack-options-parse-hilite-status params))
-          (t
-           (list params)))))
-    elem))
-
-(defun nethack-options-parse-hilite-status (params)
-  (let* ((ops (split-string params "/" t "[ \t\n\r]+"))
-         (field-name (pop ops))
-         (result (list field-name)))
-    (while ops
-      (setq result
-            (cons
-             (cond
-              ;; These two need to be handled a little differently
-              ((equal "condition" (car ops))
-               (list (cons 'condition
-                           (pop ops))   ; (cadr ops)
-                     (nethack-options-parse-attr (pop ops))))
-              ((string-suffix-p "-max" field-name)
-               (list (nethack-options-parse-attr (pop ops))))
-              ;; In most cases:
-              ((and (nethack-options-status-field-p field-name)
-                    (cdr ops))
-               (list (nethack-options-parse-status-behav (pop ops))
-                     (nethack-options-parse-attr (pop ops))))
-              ;; For something like: hilite_status:hitpoints/<=30%/red/normal
-              (t
-               (list '(else)
-                     (nethack-options-parse-attr (pop ops)))))
-             result)))
-    (reverse result)))
-
-(defun nethack-options-parse-status-behav (behav)
-  (list 'behavior
-        behav))
-
-(defun nethack-options-parse-attr (attributes)
-  (list 'attributes
-        (if (string-match-p "[&+]" attributes)
-            (split-string attributes "[&+]" t "[ \t\n\r]+")
-          attributes)))
-
-(defun nethack-options-parse-menucolor (elem)
-  "Parse a nethackrc MENUCOLOR= line.
-
-Returns an alist entry of the options set."
-  (when (string-prefix-p "MENUCOLOR=" elem)
-    (setq elem (string-trim-left elem "[a-zA-Z]+=")))
-  ;; In case the regexp contains “=”
-  ;; TODO: Do this without using ‘reverse’
-  (setq elem (reverse (split-string elem "=" t)))
-  (setq attr (pop elem))
-  (if (stringp elem)
-      (setq elem (apply #'concat (reverse elem))))
-  (setq elem (car elem))                ; There should only be 1 item
-  ;; Remove quotes from either side of ‘elem’
-  (setq elem (substring elem 1 -1))
-  (list 'menucolor
-        elem
-        (nethack-options-parse-attr attr)))
-
 
 
 (defun nethack-options-set-p (op)
   "Check if OP is set as a NetHack option.
+
+OP can be either a symbol or a string.
 
 This only really makes sense to use it on options that don't take arguments (so
 don't use it to theck “pickup types”, but do check things like “showexp”).
@@ -222,6 +215,19 @@ It can check these options, though it doesn't make sense to."
     result))
 
 
+
+(defcustom nethack-options
+  (nethack-options-parse)
+  "Alist representing nethackrc options
+
+This can be edited through ‘custom-set’."
+  ;; TODO implement customize better
+  ;; :type '(alist :key-type string)
+  :type '(sexp)
+  :group 'nethack)
+
+
+
 (provide 'nethack-options)
 
 ;;; nethack-options.el ends here
